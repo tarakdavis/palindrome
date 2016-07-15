1fde856a3f45980e0c7cc6618bdd2c402b8b65cb
Tara Davis <tkalindavis@gmail.com>
2016-07-15 13:18:40 -0400

    Updating palindrome.py, adding palindrome_test.py

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,14 @@
 import re
 
 def is_palindrome(user_sentence):
-    user_sentence = re.sub('[^a-z0-9]','', user_sentence)
+    user_sentence = re.sub('[^a-z0-9]', '', user_sentence.lower())
     if len(user_sentence) < 1:
         return False
-    if user_sentence == user_sentence[::-1]:
-        return True
-    else:
-        return False
+    return user_sentence == user_sentence[::-1]
+
 
 def main():
-    user_sentence = str(input("Enter a word, phrase, or sentence(s) to test whether it's palindromic: ").lower())
+    user_sentence = str(input("Enter a word, phrase, or sentence(s) to test whether it's palindromic: "))
     if is_palindrome(user_sentence):
         print("Your input is a palindrome!")
     else:
